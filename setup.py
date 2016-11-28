ac473a52cb81c81f44ec185aa93fa5f77b1c5f27
dantezhu <dantezhu@qq.com>
2016-11-28 21:36:59 +0800

    版本升级

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xpin",
-    version='0.1.21',
+    version='0.1.25',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
