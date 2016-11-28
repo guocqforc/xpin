f90ebf17e472ea0a75915268ab1ed21d0ecf49ab
dantezhu <dantezhu@qq.com>
2016-11-28 15:04:20 +0800

    templates要加入

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
     scripts=['xpin/bin/xpin'],
     install_requires=['requests', 'flask', 'flask_sqlalchemy', 'flask_admin', 'passlib'],
     url="https://github.com/dantezhu/xpin",
