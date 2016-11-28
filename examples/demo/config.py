885b8170cc95a08d57a54320fe909de1be0decaf
dantezhu <dantezhu@qq.com>
2016-11-28 15:53:40 +0800

    模拟配置

--- examples/demo/config.py
+++ examples/demo/config.py
@@ -11,3 +11,12 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % os.path.join(BASE_DIR, 'db.sqlite')
 
 
 CLIENT_SECRET = '123'
+
+DING_CORP_ID = ''
+DING_CORP_SECRET = ''
+DING_AGENT_ID = ''
+
+# 可选
+SEND_CLOUD_API_USER = ''
+SEND_CLOUD_API_KEY = ''
+SEND_CLOUD_SENDER = ''
