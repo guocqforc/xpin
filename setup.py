f4f16af0ad3dae46e2f09c9ecad0df233f578e77
dantezhu <dantezhu@qq.com>
2016-11-28 17:49:39 +0800

    少库

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="xpin",
-    version='0.1.5',
+    version='0.1.7',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     scripts=['xpin/bin/xpin'],
-    install_requires=['requests', 'flask', 'flask_sqlalchemy', 'flask_admin', 'passlib'],
+    install_requires=['requests', 'flask', 'flask_sqlalchemy', 'flask_admin', 'passlib', 'flask_script'],
     url="https://github.com/dantezhu/xpin",
     license="BSD",
     author="dantezhu",
